e963a2cf97696c523adb72c322b5590f465aa44e
olchu <ochurkin@gmail.com>
2026-05-28 08:41:49 +0200

    Initial Commit

---
+++ CloudeRm/CloudeRmApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudeRmApp.swift
+//  CloudeRm
+//
+//  Created by olchu on 28. 5. 2026..
+//
+
+import SwiftUI
+
+@main
+struct CloudeRmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CloudeRm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CloudeRm
+//
+//  Created by olchu on 28. 5. 2026..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
